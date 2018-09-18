65ee876d6be80a97d697a59914bd4e7cdf26dea3
hamida-mstafa <hamidamstafa@gmail.com>
2018-09-18 15:47:49 +0300

    add js

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+
+jQuery(document).ready(function() {
+  jQuery("h1").click(function() {
+    alert("This is a header.");
+  });
+
+  jQuery("p").click(function() {
+    alert("This is a paragraph.");
+  });
+
+  jQuery("img").click(function() {
+    alert("This is an image.");
+  });
+});
+$(document).ready(function() {
+  $("p").click(function() {
+    $("img").show();
+  });
+});
+// $(document).ready(function() {
+//   $("p").click(function() {
+//     $(".walrus-showing").toggle();
+//     $(".walrus-hidden").toggle();
+//   });
+// });
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $(".walrus-showing").toggle();
+    $(".walrus-hidden").toggle();
+  });
+});
+$(document).ready(function(){
+    $("button").click(function(){
+        $("p").slideToggle();
+    });
+});
